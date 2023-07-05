932251ff62b6300c5b7e0c9164245a635bb3f309
PeterPark07 <128113413+PeterPark07@users.noreply.github.com>
2023-07-05 13:48:30 +0530

    n

---
+++ src/main.py
@@ -0,0 +1,37 @@
+import os
+from flask import Flask, request
+import telebot
+from helper.log import log
+
+app = Flask(__name__)
+bot = telebot.TeleBot(os.getenv('bot_token'), threaded=False)
+bot.set_webhook(url=os.getenv('webhook_url'))
+
+# Bot route to handle incoming messages
+@app.route('/bot', methods=['POST'])
+def telegram():
+    if request.headers.get('content-type') == 'application/json':
+        json_string = request.get_data().decode('utf-8')
+        update = telebot.types.Update.de_json(json_string)
+        bot.process_new_updates([update])
+        return 'OK', 200
+
+# Handler for the '/start' command
+@bot.message_handler(commands=['start'])
+def start_command(message):
+    response_text = "Hello! Welcome to this bot!\n\n"
+    response_text += "For help, use the command /help."
+    bot.reply_to(message, response_text)
+
+# Handler for the '/help' command
+@bot.message_handler(commands=['help'])
+def help_command(message):
+    response_text = "Here are the available commands:\n\n"
+    response_text += "/start - Start the bot.\n"
+    response_text += "/help - Show this help message.\n"
+    bot.reply_to(message, response_text)
+
+# Handler for any other message
+@bot.message_handler(func=lambda message: True)
+def handle_message(message):
+    bot.reply_to(message, message.text)
