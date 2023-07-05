74d798a0edecafb9e1e1e00f22d0f9be6ff5ac93
PeterPark07 <128113413+PeterPark07@users.noreply.github.com>
2023-07-05 15:12:39 +0530

    Update main.py

--- src/main.py
+++ src/main.py
@@ -44,7 +44,10 @@ def handle_start(message):
         title = item['title']
         image = item['img']
         bot.send_photo(message.chat.id, image, caption = title)
-    bot.reply_to(message, f'/new_{(int(page) + 1)}')
+    try:
+        bot.reply_to(message, f'/new_{(int(page) + 1)}')
+    except:
+        bot.reply_to(message, 'Usage : /new_<number>')
 
 # Handler for any other message
 @bot.message_handler(func=lambda message: True)
