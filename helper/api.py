f9e145479486913d97fc7614b4c1524adeedfb7c
PeterPark07 <128113413+PeterPark07@users.noreply.github.com>
2023-07-05 14:55:48 +0530

    Update api.py

--- helper/api.py
+++ helper/api.py
@@ -2,7 +2,6 @@ import requests
 from bs4 import BeautifulSoup
 import os
 
-url = os.getenv('url')
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36',
     'Accept-Language': 'en-US,en;q=0.9',
@@ -14,6 +13,7 @@ def get_soup(url):
     return soup
     
 def latest(num):
+    url = os.getenv('url')
     url = f"{url}page/{num}"
     soup = get_soup(url)
     div_tags = soup.find_all('div', class_='tdb_module_loop td_module_wrap td-animation-stack td-cpt-post')
