7b5aa5a46c67d8781cf673b89c9d69c9a0270ae2
alwaysmaysix <104639059+alwaysmaysix@users.noreply.github.com>
2024-08-25 02:48:41 +0200

    Update api.py

--- helper/api.py
+++ helper/api.py
@@ -1,26 +1,45 @@
-import requests
+import cloudscraper
 from bs4 import BeautifulSoup
 import img2pdf
 from PIL import Image
 import os
+import random
+import json
 
-headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36',
-    'Accept-Language': 'en-US,en;q=0.9',
-}
+# Function to load user agents from browsers.json
+def load_user_agents():
+    with open('/content/cloudscraper/cloudscraper/user_agent/browsers.json', 'r') as file:
+        data = json.load(file)
+        return data['user_agents']['desktop']['windows']['chrome']
+
+# Function to create a scraper with a random user agent
+def create_random_scraper():
+    user_agents = load_user_agents()
+    random_user_agent = random.choice(user_agents)
+    scraper = cloudscraper.create_scraper(browser={
+        'browser': 'chrome',
+        'platform': 'windows',
+        'mobile': False,
+        'custom': random_user_agent
+    })
+    return scraper
+
+# Initialize scraper
+scraper = create_random_scraper()
+
 def get_soup(url):
-    response = requests.get(url, headers=headers)
+    response = scraper.get(url)
     html_content = response.text
     soup = BeautifulSoup(html_content, 'html.parser')
     return soup
-    
+
 def images_to_pdf(links_list, title):
     image_paths = []
     image_folder = 'images'
     os.makedirs(image_folder, exist_ok=True)
     n = 0
     for i, image_link in enumerate(links_list):
-        response = requests.get(image_link, headers=headers)
+        response = scraper.get(image_link)
         if response.status_code == 200:
             file_name = get_file_name(image_link)
             image_path = os.path.join(image_folder, file_name)
@@ -28,16 +47,14 @@ def images_to_pdf(links_list, title):
                 image_file.write(response.content)
             image_paths.append(image_path)
         else:
-            n+=1
+            n += 1
     pdf = f'{title}.pdf'
     with open(pdf, "wb") as f:
         f.write(img2pdf.convert(image_paths))
     return pdf, n
 
-
 def get_file_name(url):
     return url.split('/')[-1]
-
 
 ''' hr '''
 
@@ -47,7 +64,6 @@ def hr_comic_images(url):
     image_tags = content.find_all('img')
     image_links = [img.get('data-src') for img in image_tags]
     return image_links
-
 
 ''' nh '''
 
@@ -59,19 +75,14 @@ def nh_comic_images(url):
     image_links = [image_link for image_link in image_links if image_link is not None]
     return image_links
 
-
-
 ''' apc functions '''
-
-
-
 
 def apc_home():
     url = 'https://allporncomic.com/' + 'home-3/'
     soup = get_soup(url)
     content = soup.find('div', class_='page-content-listing item-big_thumbnail')
 
-    try :
+    try:
         titles = content.find_all('h3', class_='h5')
     except:
         return []
@@ -98,15 +109,14 @@ def apc_home():
 
         results.append(result)
     return results
-    
-    
+
 def apc_comic_images(url):
     soup = get_soup(url)
     content = soup.find('div', class_='reading-content')
     image_tags = content.find_all('img', class_='wp-manga-chapter-img')
     image_links = [img['data-src'].strip() for img in image_tags]
     return image_links
-    
+
 def apc_comic_info(url):
     soup = get_soup(url)
     title = soup.find('div', class_='post-title').find('h1').text.strip()
@@ -125,8 +135,7 @@ def apc_comic_info(url):
             chapter_list.append({"title": chapter_title, "url": link})
 
     return title, image_url, summary, rating, genres, chapter_list
-    
-    
+
 def apc_search(query, n):
     url = 'https://allporncomic.com/' + 'page/' + str(n) + '/?s=' + query + '&post_type=wp-manga&m_orderby=views'
     soup = get_soup(url)
@@ -135,7 +144,7 @@ def apc_search(query, n):
         results_heading = soup.find('h1', class_='h4').text.strip()
     except:
         return None, None
-        
+
     images = soup.find_all('div', class_='tab-thumb c-image-hover')
     titles = soup.find_all('div', class_='post-title')
     summaries = soup.find_all('div', class_='post-content')
@@ -161,7 +170,7 @@ def apc_search(query, n):
         chapter_link = chapter.find('a')['href']
         item_dict['chapter'] = chapter.find('a').text.strip()
         item_dict['chapter_url'] = chapter_link
-        
+
         item_dict['rating'] = rating.text.strip()
 
         results.append(item_dict)
